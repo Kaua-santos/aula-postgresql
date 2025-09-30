bb876412ca87cd797efd4de2d7e065fc6acc9b4f
kaua <kaua7santos7oliveiraa@gmail.com>
2025-09-30 11:28:24 -0300

    função deletar aluno

--- crud.py
+++ crud.py
@@ -1,32 +1,64 @@
-from db import conectar 
+from db import conectar
 
-def listar_alunos(nome, idade):
+def criar_aluno(nome, idade):
     conexao, cursor = conectar()
     if conexao:
         try:
-            cursor.execute( 
+            cursor.execute(
+                "INSERT INTO alunos (nome,idade)  VALUES (%s, %s)",
+                (nome, idade)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao inserir {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+criar_aluno("Luis Eduardo", 17)
+
+def listar_aluno(nome, idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
                 "SELECT * FROM alunos ORDER BY id"
             )
-            return cursor.fetchall()
+            conexao.commit()
         except Exception as erro:
-            print(f'erro ao listar {erro}')
+            print(f"Erro ao listar {erro}")
             return[]
         finally:
             cursor.close()
             conexao.close()
 
-def atualizar_alunos(id_aluno, nova_idade):
+
+def listar_aluno(id_aluno, nova_idade):
     conexao, cursor = conectar()
     if conexao:
         try:
             cursor.execute(
-                "UPDATE alunos SET idade = %s WHERE id %s",
+                "UPDATE alunos SET idade= %s WHERE id = %s",
                 (nova_idade, id_aluno)
             )
         except Exception as erro:
-            print(f'erro ao atualizar um aluno: {erro}')
+            print(f"Erro ao atualizar um aluno:  {erro}")
         finally:
             cursor.close()
             conexao.close()
+
+def deletar_aluno(id_aluno):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM alunos WHERE id = %s", (id_aluno,)
             
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f'erro ao deletar aluno: {erro} ')
+            cursor.close()
+            conexao.close()
+
 
